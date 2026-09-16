b96eb0f224a08fe990728cda6d0b303a09d5befa
Your Name <your.email@example.com>
2026-09-16 16:58:54 +0300

    fix(backend): import auth router in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 
-from app.api.v1.endpoints import audio, history, recommend
+from app.api.v1.endpoints import audio, history, recommend, auth
 from app.core.config import settings
 from app.core.security import limiter
 
