37c40991cd17ad082e8353866da1f6dc8f521a7b
Your Name <your.email@example.com>
2026-09-18 20:48:43 +0300

    chore(frontend): Fix all remaining flutter lints and remove unnecessary imports

--- frontend/lib/services/ambient_listening_service.dart
+++ frontend/lib/services/ambient_listening_service.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:ui';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:hive_flutter/hive_flutter.dart';
